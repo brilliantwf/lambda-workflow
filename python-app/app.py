dac59742253294a553949b0ef586f3373b847631
felix <felix@eksworkshop.com>
2024-05-29 13:15:03 +0000

    new commit

--- python-app/app.py
+++ python-app/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    return jsonify(status=200, message='hello world 11111k2!')
+    return jsonify(status=200, message='hello world 111111k2!')
