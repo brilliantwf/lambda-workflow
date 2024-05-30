98523614dfc6162797ef66cb075d870b8b4158a6
felix <felix@eksworkshop.com>
2024-05-30 04:52:10 +0000

    new commit

--- push.sh
+++ push.sh
@@ -1,7 +1,10 @@
  git update-index --skip-worktree Kustomize/
  
+ git update-index --assume-unchanged Kustomize/base/kustomization.yaml
+ git update-index --skip-worktree Kustomize/prod/*
+
  git pull --rebase origin main
  git push origin main
- git add python-app/app.py
+ git add .
  git commit -m "new commit"
  git push -u origin main
